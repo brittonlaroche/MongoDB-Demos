685baf7f2ede94dabd47d2ccf336137bdcb3e301
Britton LaRoche <32334829+brittonlaroche@users.noreply.github.com>
2019-08-22 04:34:09 -0500

    Update source.js

--- Applications/Customer-MDM/customer/functions/addressObject/source.js
+++ Applications/Customer-MDM/customer/functions/addressObject/source.js
@@ -3,25 +3,28 @@ exports = function(source){
   //Source has an adress object
   var copy = {};
   var addressCopy = {};
-  copy._id = source._id;
-  copy.first_name = source.first_name;
-  copy.middle_name = source.middle_name;
-  copy.last_name = source.last_name;
-  copy.gender = source.gender;
-  copy.dob = source.dob;
-  copy.phone = source.phone;
-  copy.email = source.email;
-  
-
   if (source) {
+      copy._id = source._id;
+      copy.first_name = source.first_name;
+      copy.middle_name = source.middle_name;
+      copy.last_name = source.last_name;
+      copy.gender = source.gender;
+      copy.dob = source.dob;
+      copy.phone = source.phone;
+      copy.email = source.email;
       if (source.address) {
-       source.address.forEach(function(myAddress) {
-          addressCopy =  myAddress;
-        });
+        if (Array.isArray(source.address)) {
+          source.address.forEach(function(myAddress) {
+            //copy the address for each element
+            //overwrite with the value from the last element
+            addressCopy =  myAddress;
+          });
+        } else {
+            //We have an object just copy it
+            addressCopy = source.address;
+        }
       }
+      copy.address = addressCopy;
   }
-  
-  copy.address = addressCopy;
-  
   return copy;
 };
